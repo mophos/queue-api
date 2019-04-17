2abb1887b634f921229aeb364136de80629443ec
Satit Riapit <siteslave@msn.com>
2019-04-17 20:52:04 +0700

    Aupdate version

--- src/app.ts
+++ src/app.ts
@@ -201,7 +201,7 @@ app.register(require('./routes/print'), { prefix: '/v1/print', logger: true });
 app.register(require('./routes/sounds'), { prefix: '/v1/sounds', logger: true });
 
 app.get('/', async (req: fastify.Request, reply: fastify.Reply) => {
-  reply.code(200).send({ message: 'Welcome to Q4U API services!', version: '2.9 build 20190319-1' })
+  reply.code(200).send({ message: 'Welcome to Q4U API services!', version: '3.0 build 20190417-1' })
 });
 
 const port = 3002;
